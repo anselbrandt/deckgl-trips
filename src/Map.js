77dd4b0b410fdbdd87ca6e481c2c353789a6629f
anselbrandt <ansel@anselbrandt.com>
2020-03-25 13:31:03 -0400

    Add GeoJsonLayer data

--- src/Map.js
+++ src/Map.js
@@ -1,10 +1,38 @@
 import React from 'react';
-import MapGL from 'react-map-gl';
+import DeckGL from '@deck.gl/react';
+import { GeoJsonLayer } from '@deck.gl/layers';
+import { StaticMap } from 'react-map-gl';
+import GeoJSON from './trip.json';
 
-export default function Map({ width, height }) {
+const MAPBOX_ACCESS_TOKEN = process.env.MAPBOX_ACCESS_TOKEN;
+
+const initialViewState = {
+  longitude: -73.675,
+  latitude: 45.551,
+  zoom: 10.6,
+  pitch: 0,
+  bearing: 0,
+};
+
+const data = GeoJSON;
+
+export default function Map() {
+  const layers = new GeoJsonLayer({
+    id: 'geojson-layer',
+    data,
+    stroked: true,
+    lineWidthScale: 1,
+    lineWidthMinPixels: 1,
+    getLineColor: [255, 99, 71],
+    getLineWidth: 1,
+  });
   return (
-    <React.Fragment>
-      <MapGL width={width} height={height} />
-    </React.Fragment>
+    <DeckGL
+      initialViewState={initialViewState}
+      controller={true}
+      layers={layers}
+    >
+      <StaticMap mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN} />
+    </DeckGL>
   );
 }
