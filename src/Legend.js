65c1d6fd6905d10ad0c59dac2cff3bb6b42b23a0
anselbrandt <ansel@anselbrandt.com>
2020-03-28 10:23:37 -0400

    Add Barchart

--- src/Legend.js
+++ src/Legend.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import styles from './Controls.module.css';
+import { VictoryBar } from 'victory';
+import jsondata from './histogram.json';
 
 export default function Legend(props) {
   const { time, handleSetTime } = props;
+  const data = jsondata;
   return (
     <div className={styles.legend} style={{ width: '80vw' }}>
       <div className={styles.slidecontainer}>
+        <VictoryBar
+          width={400}
+          height={25}
+          padding={0}
+          data={data}
+          x="bin"
+          y="count"
+          style={{ data: { fill: 'tomato' } }}
+          barRatio={1}
+        />
         <input
           className={styles.slider}
           name="time"
